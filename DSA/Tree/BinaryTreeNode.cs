e879cdad8fee741a92d60f7eb9c23c0f87631e56
Amit <amitpatel049@gmail.com>
2016-12-14 04:43:35 +0000

    adding tree project and tree node

---
+++ DSA/Tree/BinaryTreeNode.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class BinaryTreeNode<T> : TreeNode<T>
+    {
+        public BinaryTreeNode<T> Left 
+        { 
+            get
+            {
+                if (base.Neighbors != null)
+                    return (BinaryTreeNode<T>)base.Neighbors[0];
+                else
+                    return null;
+            }
+            private set
+            {
+                if(base.Neighbors==null)
+                    base.Neighbors = new List<TreeNode<T>>(2);
+                base.Neighbors[0] = value;
+            }
+        }
+
+        public BinaryTreeNode<T> Right
+        {
+            get
+            {
+                if (base.Neighbors != null)
+                    return (BinaryTreeNode<T>)base.Neighbors[1];
+                else
+                    return null;
+            }
+            private set
+            {
+                if (base.Neighbors == null)
+                    base.Neighbors = new List<TreeNode<T>>(2);
+                base.Neighbors[1] = value;
+            }
+        }
+
+        public BinaryTreeNode(T data) : base(data)
+        {
+
+        }
+        public BinaryTreeNode(T data, BinaryTreeNode<T> left, BinaryTreeNode<T> right)
+            : base(data)
+        {
+            this.Neighbors = new List<TreeNode<T>>(2);
+            this.Neighbors[0] = left;
+            this.Neighbors[1] = right;
+        }
+    }
+}
---
+++ DSA/Tree/TreeNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class TreeNode<T>
+    {
+        private T data;
+        private IList<TreeNode<T>> neighbors;
+
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        
+        public IList<TreeNode<T>> Neighbors {
+            get { return neighbors; } 
+            set{neighbors = value;}
+        }
+        public TreeNode(T data)
+        {
+            this.data = data;
+        }
+
+        public TreeNode(T data, IList<TreeNode<T>> neighbors)
+        {
+            this.data = data;
+            this.neighbors = neighbors;
+        }
+    }
+}
