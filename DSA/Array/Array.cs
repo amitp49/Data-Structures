182da5bb8d37fe8a25d29368d69c2b22dee9622e
Amit <amitpatel049@gmail.com>
2016-12-16 04:41:50 +0000

    adding array class and one method to find out lesser

---
+++ DSA/Array/Array.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    public class MyArray<T> where T : IComparable
+    {
+        private int size;
+        
+        public T[] Arr { get; set; }
+
+        public MyArray(int size)
+        {
+            this.size = size;
+            this.Arr = new T[size];
+        }
+
+        public T this[int index]
+        {
+            get { return Arr[index]; }
+            set { Arr[index] = value; }
+        }
+
+        public void PrintElementsWhichDoesntHaveAnyHigerOnTheirRight()
+        {
+            T runningMax = default(T);
+            //Lets have another array of same size to store running max from end
+            T[] another = new T[this.size];
+
+            for (int i = this.size - 1; i >= 0; i--)
+            {
+                if (runningMax.CompareTo(Arr[i]) < 0)
+                    runningMax = Arr[i];
+
+                another[i] = runningMax;
+            }
+
+            //Now we will print element only if greater or equal then another 
+            for (int i = 0; i < this.size; i++)
+            {
+                if(Arr[i].CompareTo(another[i]) >= 0)
+                    Console.WriteLine(Arr[i] + ",");
+            }
+        }
+    }
+}
--- DSA/Array/ArrayRunner.cs
+++ DSA/Array/ArrayRunner.cs
@@ -11,6 +11,12 @@ namespace Array
     {
         public void Run()
         {
+            MyArray<int> myArray = new MyArray<int>(8) 
+            { 
+                Arr = new int[] {12,15,10,11,5,6,2,3} 
+            };
+            myArray.PrintElementsWhichDoesntHaveAnyHigerOnTheirRight();
+            Console.ReadLine();
         }
     }
 }
